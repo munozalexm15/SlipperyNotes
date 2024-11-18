c07e6c7abf03f4ebea3928468c231d601b813357
Alejandro Muñoz <munozalexm15@gmail.com>
2024-11-18 22:41:16 +0100

    started home page

--- assets/vue/controllers/buttonHome.vue
+++ assets/vue/controllers/buttonHome.vue
@@ -5,14 +5,34 @@ defineProps({
 </script>
 
 <template>
-  <button class="button is-rounded is-size-5 bg">{{ text }}</button>
+  <button class="button is-rounded button-rounded-no-outline is-size-5 bg ">
+    <span class="txt-sp is-size-4">{{ text }}</span>
+  </button>
 </template>
 
 <style scoped>
 
 .bg {
-
   box-shadow: 0px 0px 0px;
+}
+
+.button-rounded-no-outline {
+  border-color: white;
+  background: none;
+}
+
+.txt-sp {
+  background-image: linear-gradient(to left, #1F9BFD, #1F73FE);
+  color: transparent;
+  background-clip: text;
+}
+
+
+button:hover {
+  border-color: #0281e8;
+  border-radius: var(--bulma-radius-rounded);
+  padding-left: calc(var(--bulma-button-padding-horizontal) + 0.25em - var(--bulma-button-border-width));
+  padding-right: calc(var(--bulma-button-padding-horizontal) + 0.25em - var(--bulma-button-border-width));
 }
 
 </style>
